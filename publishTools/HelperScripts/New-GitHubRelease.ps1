d54ff7e347855f7ccdc25d3b3fd494519f8a36ff
mwidmeyer <michael.widmeyer@gmail.com>
2017-04-10 21:02:41 -0500

    Revert "Add more parameters to Publish script and improve error handling"
    This reverts commit 005b0810722eb7a87e45b0b0201d223e0376d90b.

--- publishTools/HelperScripts/New-GitHubRelease.ps1
+++ publishTools/HelperScripts/New-GitHubRelease.ps1
@@ -8,84 +8,28 @@ function New-GitHubRelease
 	[CmdletBinding()]
 	param
 	(
-		[Parameter(Mandatory=$true,HelpMessage="The username the repository is under (e.g. deadlydog).")]
-		[string] $GitHubUsername,
+		[Parameter(Mandatory=$true,HelpMessage="The user and repository name (e.g. deadlydog/Invoke-MsBuild).")]
+		[string] $GitHubUserAndRepository,
 
-		[Parameter(Mandatory=$true,HelpMessage="The repository name to create the release in (e.g. Invoke-MsBuild).")]
-		[string] $GitHubRepositoryName,
-
-		[Parameter(Mandatory=$true,HelpMessage="The Acess Token to use as credentials for GitHub. Generate one at GitHub.com -> account Settings -> Personal Access Tokens, and make sure the token has scope repo/public_repo.")]
+		[Parameter(Mandatory=$true)]
 		[string] $GitHubAccessToken,
 
-		[Parameter(Mandatory=$true,HelpMessage="The name of the tag to create at the the CommitId.")]
-		[string] $TagName,
-
-		[Parameter(Mandatory=$false,HelpMessage="The name of the release. If blank, the TagName will be used.")]
+		[Parameter(Mandatory=$true)]
 		[string] $ReleaseName,
 
-		[Parameter(Mandatory=$false,HelpMessage="Text describing the contents of the tag.")]
+		[Parameter(Mandatory=$true)]
+		[string] $TagName,
+
 		[string] $ReleaseNotes,
 
-		[Parameter(Mandatory=$false,HelpMessage="The full paths of the files to include in the release.")]
 		[string[]] $ArtifactFilePaths,
 
-		[Parameter(Mandatory=$false, HelpMessage="Specifies the commitish value that determines where the Git tag is created from. Can be any branch or commit SHA. Unused if the Git tag already exists. Default: the repository's default branch (usually master).")]
-		[string] $CommitId, #= 'master',
-
-		[Parameter(Mandatory=$false,HelpMessage="True to create a draft (unpublished) release, False to create a published one. Default: false")]
 		[bool] $IsDraft = $false,
 
-		[Parameter(Mandatory=$false,HelpMessage="True to identify the release as a prerelease. False to identify the release as a full release. Default: false")]
 		[bool] $IsPreRelease = $false
 	)
 
-	if ([string]::IsNullOrEmpty($ReleaseName)) 
-	{ 
-		$ReleaseName = $TagName
-	}
-
-	$NewLine = [Environment]::NewLine
-
-	function Invoke-RestMethodAndThrowDescriptiveErrorOnFailure($requestParametersHashTable)
-	{
-		$requestDetailsAsNicelyFormattedString = Convert-HashTableToNicelyFormattedString $requestParametersHashTable
-		Write-Verbose "Making web request with the following parameters:$NewLine$requestDetailsAsNicelyFormattedString"
-
-		try 
-		{
-			$webRequestResult = Invoke-RestMethod @requestParametersHashTable
-		}
-		catch 
-		{
-			$exception = $_.Exception
-
-			$responseDetails = @{
-				ResponseUri = $exception.Response.ResponseUri
-				StatusCode = $exception.Response.StatusCode
-				StatusDescription = $exception.Response.StatusDescription
-				ErrorMessage = $exception.Message
-			}
-			$responseDetailsAsNicelyFormattedString = Convert-HashTableToNicelyFormattedString $responseDetails
-
-			$errorInfo = "Request Details:" + $NewLine + $requestDetailsAsNicelyFormattedString
-			$errorInfo += $NewLine
-			$errorInfo += "Response Details:" + $NewLine + $responseDetailsAsNicelyFormattedString
-			throw "An unexpected error occurred while making web request: $NewLine$errorInfo"
-		}
-
-		Write-Verbose "Web request returned the following result:$NewLine$webRequestResult"
-		return $webRequestResult
-	}
-
-	function Convert-HashTableToNicelyFormattedString($hashTable)
-	{
-		[string] $nicelyFormattedString = $hashTable.Keys | ForEach-Object {
-			$key = $_
-			$value = $hashTable.$key
-			"  $key = $value$NewLine"
-		}
-		return $nicelyFormattedString
-	}
+	$ErrorActionPreference = 'STOP'
 
 	$authHeader = 
 	@{ 
@@ -95,7 +39,7 @@ function New-GitHubRelease
 	$releaseData = 
 	@{
 		tag_name         = $TagName
-		target_commitish = $CommitId
+		target_commitish = 'master'
 		name             = $ReleaseName
 		body             = $ReleaseNotes
 		draft            = $IsDraft
@@ -104,27 +48,28 @@ function New-GitHubRelease
 
 	$createReleaseWebRequestParameters = 
 	@{
-		Uri         = "https://api.github.com/repos/$GitHubUsername/$GitHubRepositoryName/releases"
+		Uri         = "https://api.github.com/repos/$GitHubUserAndRepository/releases"
 		Method      = 'POST'
 		Headers     = $authHeader
 		ContentType = 'application/json'
-		Body        = (ConvertTo-Json $releaseData -Compress)
+		Body        = ConvertTo-Json $releaseData
 	}
 
 	Write-Information "Sending web request to create the new Release..."
-	$createReleaseWebRequestResults = Invoke-RestMethodAndThrowDescriptiveErrorOnFailure $createReleaseWebRequestParameters
+	$createReleaseWebRequestResults = Invoke-RestMethod @createReleaseWebRequestParameters
 
 	$successfulCompletionMessage = "GitHub Release creation complete! View it at: " + $createReleaseWebRequestResults.html_url
 
 	[bool] $thereAreNoArtifactsToIncludeInTheRelease = ($ArtifactFilePaths -eq $null) -or ($ArtifactFilePaths.Count -le 0)
 	if ($thereAreNoArtifactsToIncludeInTheRelease)
 	{
-		Write-Information "No artificats to include in the release. $successfulCompletionMessage"
+		Write-Output "No artificats to include in the release. $successfulCompletionMessage"
 		return
 	}
 
 	[int] $numberOfArtifactsToUpload = $ArtifactFilePaths.Count
 	[int] $numberOfArtifactsUploaded = 0
+
 	Write-Information "Uploading $numberOfArtifactsToUpload artifacts to the new release..."
 	foreach ($artifactFilePath in $ArtifactFilePaths) 
 	{
@@ -137,7 +82,6 @@ function New-GitHubRelease
 
 		$uploadArtifactWebRequestParameters = 
 		@{
-			# Upload Url has template parameters on the end (e.g. ".../assets{?name,label}"), so remove them and append the actual name.
 			Uri         = ($createReleaseWebRequestResults.upload_url -replace '{.+}') + "?name=$artifactFileName"
 			Method      = 'POST'
 			Headers     = $authHeader
@@ -147,8 +91,8 @@ function New-GitHubRelease
 
 		$numberOfArtifactsUploaded = $numberOfArtifactsUploaded + 1
 		Write-Information "Uploading artifact $numberOfArtifactsUploaded of $numberOfArtifactsToUpload, '$artifactFilePath'."
-		Invoke-RestMethodAndThrowDescriptiveErrorOnFailure $uploadArtifactWebRequestParameters > $null
+		Invoke-RestMethod @uploadArtifactWebRequestParameters > $null
 	}
 
-	Write-Information $successfulCompletionMessage
+	Write-Output $successfulCompletionMessage
 }
